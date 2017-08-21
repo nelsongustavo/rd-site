3a40f1c29c816550bcf79d785d27aa647b8eb79a
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-21 19:32:09 -0300

    navigation

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <h2>Welcome to Home</h2>
+        <ul>
+          <li><Link to="/price">Price</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </div>
     );
   }
